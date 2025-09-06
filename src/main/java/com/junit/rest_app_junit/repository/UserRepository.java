11018b424cc02a1d0551216f0f08426dadc2afc4
Hitesh ahire <hbahire2014@gmail.com>
2025-09-06 23:48:50 +0530

    modified some files
    Signed-off-by: Hitesh ahire <hbahire2014@gmail.com>

--- src/main/java/com/junit/rest_app_junit/repository/UserRepository.java
+++ src/main/java/com/junit/rest_app_junit/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.junit.rest_app_junit.repository;
 
+import com.junit.rest_app_junit.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
